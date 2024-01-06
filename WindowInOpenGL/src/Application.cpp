97851b48f9690c2a17731e90642052f7b317de17
theDRB123 <dhrbhanushali123@gmail.com>
2024-01-06 15:14:35 +0530

    added shader abstraction !!

--- WindowInOpenGL/src/Application.cpp
+++ WindowInOpenGL/src/Application.cpp
@@ -10,81 +10,81 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
+#include "Shader.h"
 
-
-struct ShaderProgramSource {
-	std::string VertexSource;
-	std::string FragmentSource;
-};
-
-static ShaderProgramSource ParseShader(const std::string& filepath) {
-
-	std::ifstream stream(filepath);
-
-	enum class ShaderType {
-		NONE = -1 , VERTEX = 0 , FRAGMENT = 1
-	};
-
-	std::string line;
-	std::stringstream ss[2];
-
-	ShaderType type = ShaderType::NONE;
-
-	while (getline(stream, line)) {
-		
-		if (line.find("#shader") != std::string::npos) {
-			if (line.find("vertex") != std::string::npos) {
-				type = ShaderType::VERTEX;
-			}
-			if (line.find("fragment") != std::string::npos) {
-				type = ShaderType::FRAGMENT;
-			}
-		}
-		else {
-			ss[(int)type] << line << '\n';
-		}
-	}
-	return { ss[0].str()  , ss[1].str() };
-}
-
-static unsigned int CompileShader( unsigned int type , const std::string& source ) {
-	unsigned int id = glCreateShader(type);
-	
-	const char* src = source.c_str();//this will return a pointer to the beginning of the string (c_str() returns the pointer to the beginning of the string)
-	
-	glShaderSource( id , 1 , &src , nullptr );
-	glCompileShader( id );
-	int result;
-	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	if (result == GL_FALSE) {
-		int length;
-		glGetShaderiv(id , GL_INFO_LOG_LENGTH , &length );
-		char* message = (char*)alloca(length * sizeof(char));
-		glGetShaderInfoLog(id, length, &length, message);
-		std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
-		std::cout << message << std::endl;
-		glDeleteShader(id);
-		return 0;
-	}
-
-	return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
-	unsigned int program = glCreateProgram();
-	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-	
-	glAttachShader(program, vs);
-	glAttachShader(program, fs);
-	glLinkProgram(program);
-	glValidateProgram(program);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-
-	return program;
-}
+//struct ShaderProgramSource {
+//	std::string VertexSource;
+//	std::string FragmentSource;
+//};
+//
+//static ShaderProgramSource ParseShader(const std::string& filepath) {
+//
+//	std::ifstream stream(filepath);
+//
+//	enum class ShaderType {
+//		NONE = -1 , VERTEX = 0 , FRAGMENT = 1
+//	};
+//
+//	std::string line;
+//	std::stringstream ss[2];
+//
+//	ShaderType type = ShaderType::NONE;
+//
+//	while (getline(stream, line)) {
+//		
+//		if (line.find("#shader") != std::string::npos) {
+//			if (line.find("vertex") != std::string::npos) {
+//				type = ShaderType::VERTEX;
+//			}
+//			if (line.find("fragment") != std::string::npos) {
+//				type = ShaderType::FRAGMENT;
+//			}
+//		}
+//		else {
+//			ss[(int)type] << line << '\n';
+//		}
+//	}
+//	return { ss[0].str()  , ss[1].str() };
+//}
+//
+//static unsigned int CompileShader( unsigned int type , const std::string& source ) {
+//	unsigned int id = glCreateShader(type);
+//	
+//	const char* src = source.c_str();//this will return a pointer to the beginning of the string (c_str() returns the pointer to the beginning of the string)
+//	
+//	glShaderSource( id , 1 , &src , nullptr );
+//	glCompileShader( id );
+//	int result;
+//	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+//	if (result == GL_FALSE) {
+//		int length;
+//		glGetShaderiv(id , GL_INFO_LOG_LENGTH , &length );
+//		char* message = (char*)alloca(length * sizeof(char));
+//		glGetShaderInfoLog(id, length, &length, message);
+//		std::cout << "Failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader!" << std::endl;
+//		std::cout << message << std::endl;
+//		glDeleteShader(id);
+//		return 0;
+//	}
+//
+//	return id;
+//}
+//
+//static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+//	unsigned int program = glCreateProgram();
+//	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+//	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+//	
+//	glAttachShader(program, vs);
+//	glAttachShader(program, fs);
+//	glLinkProgram(program);
+//	glValidateProgram(program);
+//
+//	glDeleteShader(vs);
+//	glDeleteShader(fs);
+//
+//	return program;
+//}
 
 
 
@@ -93,12 +93,10 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
 
 	GLFWwindow* window;
-	
-	/* Initialize the library */
+
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(600, 600, "This is my triangle", NULL, NULL);
 	if (!window)
 	{
@@ -106,9 +104,7 @@ int main(void)
 		return -1;
 	}
 
-	//int nrAttributes;
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
 	glewInit();
@@ -133,7 +129,7 @@ int main(void)
 	};
 
 
-	//enabling the debug output
+
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback(MessageCallback, 0);
 
@@ -150,47 +146,34 @@ int main(void)
 
 	IndexBuffer ib(indices, 6);
 	
-	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-
-
-
-		
-	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-	glUseProgram(shader);
-
-	// after using the required shader, we will define the uniforms for it
-	// as it is required that we have a binded shader before we can define the uniforms
-
-	int location = glGetUniformLocation(shader, "u_shift");
-	//ASSERT(location != -1);
-	//glUniform4f(location, 0.0f, 0.0f, 0.0f, 1.0f);
-
+	/*ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);*/
+	Shader shader("res/shaders/Basic.shader");
+	shader.Bind();
+	/*glUseProgram(shader);
+	int location = glGetUniformLocation(shader, "u_shift");*/
+	//shader.SetUniform1f(u_shift)
 	float shift_val = 0.0f;
 	float increment = 0.005f;
 
 
-	// unbinding all the attributes before the loop
-	glBindVertexArray(0);
-	glUseProgram(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	//glBindVertexArray(0);
+	va.Unbind();
+	vb.Unbind();
+	ib.Unbind();
+	shader.Unbind();
+	
 
-
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader);
-		//glBindBuffer(GL_ARRAY_BUFFER, buffer);
-		//glBindVertexArray(vao);
+		shader.Bind();
 		va.Bind();
 		ib.Bind();
-		/*glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);*/
+	
 
-		glUniform1f(location, shift_val);
+		shader.SetUniform1f("u_shift", shift_val);
 		if (shift_val > 0.5f) {
 			increment = -0.005f;
 		}
@@ -199,19 +182,11 @@ int main(void)
 		}
 		shift_val += increment;
 
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		//how can i draw a square?
-		
-		//note: we need to write GLCall inside GLCall  
-		glDrawElements(GL_TRIANGLES, 6 , GL_UNSIGNED_INT , nullptr);
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
 
-		/* Poll for and process e-vents */
+		glDrawElements(GL_TRIANGLES, 6 , GL_UNSIGNED_INT , nullptr);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
-	//glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
